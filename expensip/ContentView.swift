b89a1c04cd50ea4aa4cb42bdac5a64ab79736d7b
berkay <berkayoruc@hotmail.com>
2022-09-04 23:31:01 +0300

    first commit

--- expensip/ContentView.swift
+++ expensip/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var score: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("ss", value: $score, formatter: AAA())
+                .keyboardType(.decimalPad)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+class AAA: Formatter {
+    override func string(for obj: Any?) -> String? {
+        if let string = obj as? String {
+            return formatted(value: string)
+        }
+        return nil
+    }
+    
+    override func getObjectValue(_ obj: AutoreleasingUnsafeMutablePointer<AnyObject?>?, for string: String, errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>?) -> Bool {
+        obj?.pointee = string as AnyObject?
+        return true
+    }
+    
+    func formatted(value: String?) -> String? {
+        guard let aa = value else { return nil }
+        return aa
     }
 }
