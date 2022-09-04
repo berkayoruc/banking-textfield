7159da73cdacf8652a2c94a7b53dbe4340366e57
berkay <berkayoruc@hotmail.com>
2022-09-04 23:02:13 +0300

    Initial Commit

---
+++ expensip/expensipApp.swift
@@ -0,0 +1,17 @@
+//
+//  expensipApp.swift
+//  expensip
+//
+//  Created by berkay on 4.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct expensipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
